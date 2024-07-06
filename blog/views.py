64d5833b292e06275262c857cf3448683033ce86
Soloquite <fajanaayomide8@gmail.com>
2024-07-06 14:38:13 +0100

    some other minor changes

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
+
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
     return render(request, 'blog/index.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
